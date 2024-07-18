c74b9f4be416b31f4faa21a4752657e234960a1f
Liz-cloud <mafunulinda@outlook.com>
2024-07-18 15:06:34 +0100

    receive can messages

--- can_-receive.py
+++ can_-receive.py
@@ -3,13 +3,14 @@ import can
 bus = can.interface.Bus(channel='can0', bustype='socketcan') 
 
 def receive_can_message(): 
-
+    count=0
     while True: 
-
-        message = bus.recv() 
-
-        print(f"Message received: {message}") 
-
+        if count <10:
+            message = bus.recv() 
+            print(f"Message received: {message}") 
+            count+=1
+        else:
+            print('--------First 10 Messages Received -------')
 
 
 if __name__ == "__main__": 
